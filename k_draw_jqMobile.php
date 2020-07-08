e836a15ec38894dce31bc27867c25c543e556430
Kevin <kevincdunn.kcd@gmail.com>
2020-07-08 11:05:03 -0500

    adjusted jqmobile and css to k_draw

--- k_draw_jqMobile.php
+++ k_draw_jqMobile.php
@@ -4,66 +4,61 @@ echo <<<_BODY
 <html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv='X-UA-Compatible" content='IE+edge'>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>K Draw</title>
 
+        
+        <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+
         <link rel='stylesheet' href='jquery-mobile/jquery.mobile-1.4.5.min.css'>
-        <script src="js/jquery-3.5.0.min.js"></script>
-        <script> src='jquery-mobile/jquery.mobile-1.4.5.min.js'</script>
+        <script src='js/jquery-2.2.4.min.js'></script>
+        <script src='jquery-mobile/jquery.mobile-1.4.5.min.js'></script>
         
         <link rel="stylesheet" href="css/kdrawNew.css">
         
-        <script>
-            var _hmt = _hmt || []; (function() {
-                var hm = document.createElement('script');
-                hm.src = '//hm.baidu.com/hm.js?73c27e26f610eb3c9f3feb0c75b03925';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(hm, s);
-            })();
-        </script>
+        
     </head>
 <body>
 
 
-<div data-role='page' id='pageone'>
-    <div data-role='header'>
+<div data-role='page' id='page'>
+    <div data-role="header" id="header">
         <h1>K-Draw</h1>
     </div>
-    <div data-role='content' class='ui-content'>
-        <div id='colorPallet' class='' style='padding-bottom: 3px;'>
-            <h3>Color Pallet</h3>
-            <div id='colorPicker'>
-                <button id="black" class='lineColor' onclick="lineColor('black')" roll='button'><span></span> Black</button>
-                <button id="red" class='lineColor' onclick="lineColor('red')" role='button'><span></span> Red</button>
-                <button id="orange" class='lineColor' onclick="lineColor('orange')" role='button'><span></span> Orange</button>
-                <button id="yellow" class='lineColor' onclick="lineColor('yellow')" role='button'><span></span> Yellow</button>
-                <button id="green" class='lineColor' onclick="lineColor('green')" role='button'><span></span> Green</button>
-                <button id="blue" class='lineColor' onclick="lineColor('blue')" role='button'><span></span> Blue</button>
-                <button id="purple" class='lineColor' onclick="lineColor('purple')" role='button'><span></span> Purple </button>
-                <button id="white" class='lineColor' onclick="lineColor('white')" role='button'><span></span> white </button>
-            </div>
-        </div>
-        <div>
-            <div id="canvasDiv">
-                
-            </div>
+
+    <h3>Color Pallet: </h3>
+    <fieldset id='colorPicker' data-role="controlgroup" data-type="horizontal" data-mini="true">
+    
+        <button id="black" class='lineColor ui-btn' onclick="lineColor('black')" data-role='button'><span></span> Black</button>
+        <button id="red" class='lineColor ui-btn' onclick="lineColor('red')" data-role='button'><span></span> Red</button>
+        <button id="orange" class='lineColor ui-btn' onclick="lineColor('orange')" data-role='button'><span></span> Orange</button>
+        <button id="yellow" class='lineColor ui-btn' onclick="lineColor('yellow')" data-role='button'><span></span> Yellow</button>
+        <button id="green" class='lineColor ui-btn' onclick="lineColor('green')"><span></span> Green</button>
+        <button id="blue" class='lineColor ui-btn' onclick="lineColor('blue')"><span></span> Blue</button>
+        <button id="purple" class='lineColor ui-btn' onclick="lineColor('purple')"><span></span> Purple </button>
+        <button id="white" class='lineColor ui-btn' onclick="lineColor('white')"><span></span> white </button>
+    </fieldset>
+    
+    <div>
+        <div id="canvasDiv">
             
         </div>
-    
-    
         
-        <div id='sizePallet' class='' style='padding-bottom: 3px;'>
-            <h3>Line Size</h3>
-            <button id="small" class='lineSize' onclick="lineSize(2)">Small   <span></span></button>
-            <button id="medium" class='lineSize' onclick="lineSize(4)">Medium <span></span></button>
-            <button id="large" class='lineSize' onclick="lineSize(8)">Large   <span></span></button>
-        </div>
-    
-        <br/>
     </div>
+    
+    
+    <h3>Line Size: </h3>
+    <fieldset id='sizePallet' data-role="controlgroup" data-type="horizontal" data-mini="true">
+        
+        <button id="small" class='lineSize ui-btn' onclick="lineSize(2)">Small   <span></span></button>
+        <button id="medium" class='lineSize ui-btn' onclick="lineSize(4)">Medium <span></span></button>
+        <button id="large" class='lineSize ui-btn' onclick="lineSize(8)">Large   <span></span></button>
+    </fieldset>
+
+    <br/>
     <a id='reset' href="k_draw.php">Reset</a>
-</div>
+
 
         
         
